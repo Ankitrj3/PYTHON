7701cf872b8b5ce518d7bd410f38fb62584e479d
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-03-28 14:00:43 +0530

    Add files via upload
    tuple and sets

---
+++ tupleset.py
@@ -0,0 +1,26 @@
+## tuples 
+##ank=(12,3,57,56,34)
+##print(ank) 
+
+##ank.insert(2,35) #this code will through error beacuse tuple can not be edited when it created
+##print(ank)
+
+## functions of tuples are count,index
+##print(ank.count(12))
+##print(ank.index(57))
+
+## sets
+ank={12,34,5,11,56,89,98,76}
+##print(ank)
+#### these sets are unoredered
+##print(ank.index(34))## it will through the error because the set is unoredered and it change the item index every time
+
+print(len(ank))
+print(type(ank))
+print(ank.pop())
+print(ank)
+##print(ank.sort())##it can not run because it sets are unoredered
+print(ank.clear())
+print(ank)
+
+
