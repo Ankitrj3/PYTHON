424297ae6db0db623bb27684acdaf21dc2faf56c
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-03-29 12:45:58 +0530

    DICTIONARY

---
+++ dict.py
@@ -0,0 +1,31 @@
+##value={'name':'ankitrj',
+##       'course':'python',
+##       'level':'beginners'}
+##print(value)
+##
+##a=[1,2,3,4,5,6]
+##b=['a','b','c','d','e','f']
+##c=dict(zip(a,b))
+##print(c)
+##
+##print(type(c))
+##
+##print(c.get(1,'not found'))##it will return the a because we have defined the value in dictionary
+##print(c.get(8,'not found'))##it will print not found because we haven't define 7 key value pair
+##
+##
+##ank={'ank':'rj','p':'k','mom':'dad','num':[1,2,3,4,5,6,7,8,9],'dic':{'rk':'robin','aj':'rj','name':'ankit'}}
+##print(ank)
+
+
+
+ank=[12000,13000,14000,15000]
+rj=['ankit','robin','aman','paarth']
+dic=dict(zip(ank,rj))
+print(dic)
+
+print(dic.get(16600,'not found'))
+print(dic.get(12000,'not found'))
+print(dic.get(13000,'not found'))
+print(dic.get(13022,'not found'))
+
