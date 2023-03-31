7ad7679ac9f808f203a8c4e8a8169c21afe33399
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-03-31 12:58:24 +0530

    Variables and memory address

---
+++ variable.py
@@ -0,0 +1,28 @@
+num=50
+print(id(num))
+name="ankitranjan"
+print(id(name))
+
+a=12
+b=a
+print(a)
+print(b)
+print(id(b))##in python we if we are assinging same in two different variable it required  one space of memory for two variable
+print(id(b))
+
+c=23
+d=23
+print(id(c))##in python we if we are assinging same in two different variable it required  one space of memory for two variable
+print(id(d))
+
+f=12
+print(id(f))#python is more memory efficient
+a=3
+print(id(a))# now we get different address
+
+pi=3.14
+print(pi)
+#in python we can not create the constant
+
+print(type(pi))
+
