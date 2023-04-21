5cdbd085eaa194db007fdca0277cb3876e7cdfe8
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-04-21 14:09:52 +0530

    GST Program Using Function

---
+++ gstprogram.py
@@ -0,0 +1,14 @@
+##def gstcal():
+##    a=int(input("enter the orginal cost of the product"))
+##    b=int(input("enter the tax percentage of the product"))
+##    gstresult=(a*b)/100
+##    print('your total gst bill is' ,gstresult)
+##
+##gstcal()
+## with argument
+def gstcal(a,b):
+
+    gstresult=(a*b)/100
+    print('your total gst bill is' ,gstresult)
+
+gstcal(1900,89)
