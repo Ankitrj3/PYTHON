3f5e3bd8a465ed3a986f904c78aa604363dada11
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-04-23 00:53:35 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class calculation:
+    def addition(self,a,b):
+        return a+b
+class calulation1(calculation):
+    def subtraction(self,a,b):
+        return a-b
+ob1=calulation1()
+
+print(ob1.subtraction(122,45))
+
+
