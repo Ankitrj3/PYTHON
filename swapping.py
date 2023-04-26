3b849165e8412f2e74a3cc3c907575cf648c2630
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-04-26 22:56:08 +0530

    swapping of two numbers in python using 3 techniques

---
+++ swapping.py
@@ -0,0 +1,28 @@
+##a=12
+##b=23
+##
+##temp=a
+##a=b
+##b=temp
+##print('a',a)
+##print('b',b)
+
+
+##without using third variable
+##a=5
+##b=7
+##
+##a=a+b
+##b=a-b
+##a=a-b
+##
+##print(a,'a')
+##print(b,'b')
+
+
+a=8
+b=89
+a,b = b,a
+
+print(a)
+print(b)
