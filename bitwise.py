0f0a8dfc84a172c3afdf9ecdc2519b3e968ce99a
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-06-04 20:23:34 +0530

    bitwise operator

---
+++ bitwise.py
@@ -0,0 +1,28 @@
+##bitwise operator
+##compilent operator ~
+a=~12
+print(a)
+b=~1
+print(b)
+
+## bitwise and operator &
+a=12 & 14
+print(a)
+
+b=25 & 30
+print(b)
+## bitwise or operator |
+a=12 | 23
+print(a)
+
+## bitwise xor operator ^
+c=12^13
+print(c)
+
+##left shift << gaining the bits 
+k=10<<2
+print(k)
+
+##right shift >> loosing the bits
+k=20>>2
+print(k)
